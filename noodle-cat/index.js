46d96400c60c4b047973bb84f460e37d32533b7f
Anna Chernyakova <anchernyakova@yandex.ru>
2023-10-06 03:50:06 +0400

    feat: add js for button play, randomtime and randoholl for cat

--- noodle-cat/index.js
+++ noodle-cat/index.js
@@ -1,64 +1,96 @@
-const pImage = document.querySelector('.p_image');
-const opImage = document.querySelector('.op_image');
+
 const audio = document.querySelector("audio");
+const cats = document.querySelectorAll('.cat');
 
-pImage.addEventListener('click', function() {
-    pImage.style.display = 'none';
-    opImage.style.display = 'block';
+cats.forEach(function(cat) {
+    cat.addEventListener('click', function() {
+        // Здесь вы можете получить доступ к p_image и op_image для этого конкретного 'cat'
+        const pImage = cat.querySelector('.p_image');
+        const opImage = cat.querySelector('.op_image');
+     
+        pImage.style.display = 'none';
+        opImage.style.display = 'block';
 
-    // Воспроизведение звука
-    audio.play();
+        // Воспроизведение звука
+        audio.play();
 
-    // Задержка на 1 секунду перед возвратом к начальному состоянию
-    setTimeout(function() {
-        pImage.style.display = 'block';
-        opImage.style.display = 'none';
-    }, 100); // 1000 миллисекунд = 1 секунда
+        // Задержка на 1 секунду перед возвратом к начальному состоянию
+        setTimeout(function() {
+            pImage.style.display = 'block';
+            opImage.style.display = 'none';
+        }, 200); // 1000 миллисекунд = 1 секунда
+    });
 });
+
+
+cats.forEach(function(cat) {
+    cat.addEventListener('click', function() {
+        setTimeout(function() {
+                 
+            // Восстанавливаем видимость блока '.cat'
+            cat.style.display = 'none';
+        }, 250); // 1000 миллисекунд = 1 секунда
+
+    })
+});
+
 function randomTime(minValue, maxValue) {
     return Math.round(Math.random() * (maxValue - minValue) + minValue);
 }
 
-let min = 150;
-let max = 1000;
-
-let randomResult = randomTime(min, max);
 
 
 const cat = document.querySelectorAll('.cat');
-const holl = document.querySelectorAll('.column');
+const shadow = document.querySelectorAll('.overlay_image2');
 
-function randomHoll(holl) {
-    const i = Math.floor(Math.random() * holl.length);
-    resultHoll = holl[i];
-    return resultHoll;
+function randomCat(cat) {
+    const i = Math.floor(Math.random() * cat.length);
+    const result = cat[i];
+    return result;
 }
 
-const randomColumn = randomHoll(holl);
-console.log(randomColumn);
+function pop() {
+    let min = 300;
+    let max = 9000;
+    let randomResult = randomTime(min, max);
 
+    const randomSelectedCat = randomCat(cat); 
+    const time = randomResult;
+    randomSelectedCat.classList.add('cat_up');
 
+    // Применяем класс 'cat_up' к каждому элементу с классом 'overlay_image2'
+    shadow.forEach(function (element) {
+        element.classList.add('cat_up');
+    });
 
+    setTimeout(function () {
+        randomSelectedCat.classList.remove('cat_up');
 
+        // Удаляем класс 'cat_up' у каждого элемента с классом 'overlay_image2'
+        shadow.forEach(function (element) {
+            element.classList.remove('cat_up');
+        });
 
-
-
-
-
-
+        if (gamePlaying) {
+            pop();
+        }
+    }, time);
+}
 
 
 const playButton = document.querySelector(".play-button");
-
+let gamePlaying = false;
 
 playButton.addEventListener("click", () => {
-    if (!songPlaying) {
+    if (!gamePlaying) {
         playButton.classList.remove("play");
         playButton.classList.add("pause");
-      
+        gamePlaying = true;
+        // Вызывайте функцию `pop` только после нажатия кнопки play
+        pop();
     } else {
         playButton.classList.remove("pause");
         playButton.classList.add("play");
-      
+        gamePlaying = false;
     }
 });
