caeb526426aafe678f5b2c6f942a11b476375e2c
Anna Chernyakova <anchernyakova@yandex.ru>
2023-10-08 04:07:51 +0400

    feat: add js for modal

--- noodle-cat/index.js
+++ noodle-cat/index.js
@@ -1,3 +1,5 @@
+let gameResults = JSON.parse(localStorage.getItem("gameResults")) || [];
+
 
 const audio = document.querySelector("audio");
 const cats = document.querySelectorAll('.cat');
@@ -112,21 +114,68 @@ playButton.addEventListener("click", () => {
     } else {
         pauseTimer();
     }
-    setTimeout(() => {
-        if (gamePlaying === true) { // Используйте === для сравнения
-            playButton.classList.remove("pause");
-            playButton.classList.add("play");
-            modalUp.style.display = "flex";
-            localStorage.setItem("score", score);
-            score = 0;
-            getScore();
-            gamePlaying = false;
-            clearInterval(intervalId); // Остановить интервал
-            intervalId = null;
-            resetTimer();
-        }
-    }, 6000);
+    setTimeout(endGameAndSaveResults, 12000);
 });
+
+function endGameAndSaveResults() {
+    if (gamePlaying === true) {
+        playButton.classList.remove("pause");
+        playButton.classList.add("play");
+        modalUp.style.display = "flex";
+        gameResults.push(score);
+        localStorage.setItem("gameResults", JSON.stringify(gameResults));
+        score = 0;
+        getScore();
+        gamePlaying = false;
+        clearInterval(intervalId);
+        intervalId = null;
+        resetTimer();
+        const storedData = JSON.parse(localStorage.getItem("gameResults"));
+const lastResult = storedData[storedData.length - 1];
+const finalScore = document.querySelector(".final_score");
+finalScore.textContent = `You popped ${lastResult} cat for 2 seconds`;
+
+const oneResult = storedData[storedData.length - 2];
+const oneScore = document.querySelector(".one");
+oneScore.textContent = `1  you popped ${oneResult} cat`;
+
+const twoResult = storedData[storedData.length - 3];
+const twoScore = document.querySelector(".two");
+twoScore.textContent = `2  you popped ${twoResult} cat`;
+
+const threResult = storedData[storedData.length - 4];
+const threScore = document.querySelector(".thre");
+threScore.textContent = `3  you popped ${threResult} cat`;
+
+const foreResult = storedData[storedData.length - 5];
+const foreScore = document.querySelector(".fore");
+foreScore.textContent = `4  you popped ${foreResult} cat`;
+
+const fiveResult = storedData[storedData.length - 6];
+const fiveScore = document.querySelector(".five");
+fiveScore.textContent = `5  you popped ${fiveResult} cat`;
+
+const sixResult = storedData[storedData.length - 7];
+const sixScore = document.querySelector(".six");
+sixScore.textContent = `6  you popped ${sixResult} cat`;
+
+const sevenResult = storedData[storedData.length - 8];
+const sevenScore = document.querySelector(".seven");
+sevenScore.textContent = `7  you popped ${sevenResult} cat`;
+
+const eightResult = storedData[storedData.length - 9];
+const eightScore = document.querySelector(".eight");
+eightScore.textContent = `8  you popped ${eightResult} cat`;
+
+const nineResult = storedData[storedData.length - 10];
+const nineScore = document.querySelector(".nine");
+nineScore.textContent = `9  you popped ${nineResult} cat`;
+
+const tenResult = storedData[storedData.length - 11];
+const tenScore = document.querySelector(".ten");
+tenScore.textContent = `10  you popped ${tenResult} cat`;
+    }
+}
 
 function pauseTimer() {
     clearInterval(intervalId); // Остановить интервал
@@ -160,3 +209,5 @@ const exit = document.querySelector(".exit");
 exit.addEventListener("click", () => {
     modalUp.style.display = "none";
 });
+
+
