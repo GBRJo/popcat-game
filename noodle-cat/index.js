ce62f06c9bf2841f886f55a10c9e760c84fe7510
Anna Chernyakova <anchernyakova@yandex.ru>
2023-10-08 09:00:05 +0400

    fix: top of the score if no resalts

--- noodle-cat/index.js
+++ noodle-cat/index.js
@@ -1,32 +1,25 @@
 let gameResults = JSON.parse(localStorage.getItem("gameResults")) || [];
 
-
 const audio = document.querySelector("audio");
 const cats = document.querySelectorAll('.cat');
 let score = 0;
 
-
 cats.forEach(function(cat) {
     cat.addEventListener('click', function() {
-        // Здесь вы можете получить доступ к p_image и op_image для этого конкретного 'cat'
         const pImage = cat.querySelector('.p_image');
         const opImage = cat.querySelector('.op_image');
      
         pImage.style.display = 'none';
         opImage.style.display = 'block';
 
-        // Воспроизведение звука
         audio.play();
         score++;
-
-        // Обновление счета
         getScore();
 
-        // Задержка на 1 секунду перед возвратом к начальному состоянию
         setTimeout(function() {
             pImage.style.display = 'block';
             opImage.style.display = 'none';
-        }, 90); // 1000 миллисекунд = 1 секунда
+        }, 90);
     });
 });
 
@@ -35,25 +28,19 @@ function getScore() {
     scoreCount.innerHTML = score;
 }
 
-// Вызываем getScore() в начале, чтобы отобразить начальное значение счета
 getScore();
 
 cats.forEach(function(cat) {
     cat.addEventListener('click', function() {
         setTimeout(function() {
-                 
-            // Восстанавливаем видимость блока '.cat'
             cat.style.display = 'none';
-        }, 280); // 1000 миллисекунд = 1 секунда
-
+        }, 280);
     })
 });
 
 function randomTime(minValue, maxValue) {
     return Math.round(Math.random() * (maxValue - minValue) + minValue);
 }
-
-
 
 const cat = document.querySelectorAll('.cat');
 const shadow = document.querySelectorAll('.overlay_image2');
@@ -73,7 +60,6 @@ function pop() {
     const time = randomResult;
     randomSelectedCat.classList.add('cat_up');
 
-    // Применяем класс 'cat_up' к каждому элементу с классом 'overlay_image2'
     shadow.forEach(function (element) {
         element.classList.add('cat_up');
     });
@@ -81,7 +67,6 @@ function pop() {
     setTimeout(function () {
         randomSelectedCat.classList.remove('cat_up');
 
-        // Удаляем класс 'cat_up' у каждого элемента с классом 'overlay_image2'
         shadow.forEach(function (element) {
             element.classList.remove('cat_up');
         });
@@ -96,7 +81,7 @@ const modalUp = document.querySelector(".modal");
 const playButton = document.querySelector(".play-button");
 let gamePlaying = false;
 
-let intervalId; // Переменная для хранения идентификатора интервала
+let intervalId;
 let currentTime = 0;
 
 playButton.addEventListener("click", () => {
@@ -108,8 +93,7 @@ playButton.addEventListener("click", () => {
             currentTime++;
             const progressTime = document.querySelector(".current");
             progressTime.textContent = getTime(currentTime);
-        }, 100); // Интервал в 100 миллисекунд (0.1 секунды)
-        // Вызывайте функцию `pop` только после нажатия кнопки play
+        }, 100);
         pop();
     } else {
         pauseTimer();
@@ -131,59 +115,100 @@ function endGameAndSaveResults() {
         intervalId = null;
         resetTimer();
         const storedData = JSON.parse(localStorage.getItem("gameResults"));
-const lastResult = storedData[storedData.length - 1];
-const finalScore = document.querySelector(".final_score");
-finalScore.textContent = `You popped ${lastResult} cat for 2 seconds`;
+        const lastResult = storedData[storedData.length - 1];
+        const finalScore = document.querySelector(".final_score");
+        finalScore.textContent = `You popped ${lastResult} cat for 2 seconds`;
 
-const oneResult = storedData[storedData.length - 2];
-const oneScore = document.querySelector(".one");
-oneScore.textContent = `1  you popped ${oneResult} cat`;
+        const oneScore = document.querySelector(".one");
+        if (storedData.length >= 2) {
+            const oneResult = storedData[storedData.length - 2];
+            oneScore.textContent = `1 - you popped ${oneResult} cat`;
+        } else {
+            oneScore.textContent = `1 - `;
+        }
+   
+        const twoScore = document.querySelector(".two");
+        if (storedData.length >= 3) {
+            const twoResult = storedData[storedData.length - 3];
+            twoScore.textContent = `2 - you popped ${twoResult} cat`;
+        } else {
+            twoScore.textContent = `2 - `;
+        }
 
-const twoResult = storedData[storedData.length - 3];
-const twoScore = document.querySelector(".two");
-twoScore.textContent = `2  you popped ${twoResult} cat`;
+        const threScore = document.querySelector(".thre");
+        if (storedData.length >= 4) {
+            const threResult = storedData[storedData.length - 4];
+            threScore.textContent = `3 - you popped ${threResult} cat`;
+        } else {
+            threScore.style.display = "none";
+        }
+        
+        const foreScore = document.querySelector(".fore");
+        if (storedData.length >= 5) {
+            const foreResult = storedData[storedData.length - 5];
+            foreScore.textContent = `4 - you popped ${foreResult} cat`;
+        } else {
+            foreScore.style.display = "none";
+        }
+        
+        const fiveScore = document.querySelector(".five");
+        if (storedData.length >= 6) {
+            const fiveResult = storedData[storedData.length - 6];
+            fiveScore.textContent = `5 - you popped ${fiveResult} cat`;
+        } else {
+            fiveScore.style.display = "none";
+        }
 
-const threResult = storedData[storedData.length - 4];
-const threScore = document.querySelector(".thre");
-threScore.textContent = `3  you popped ${threResult} cat`;
+        const sixScore = document.querySelector(".six");
+        if (storedData.length >= 7) {
+            const sixResult = storedData[storedData.length - 7];
+            sixScore.textContent = `6 - you popped ${sixResult} cat`;
+        } else {
+            sixScore.style.display = "none";
+        }
 
-const foreResult = storedData[storedData.length - 5];
-const foreScore = document.querySelector(".fore");
-foreScore.textContent = `4  you popped ${foreResult} cat`;
+        const sevenScore = document.querySelector(".seven");
+        if (storedData.length >= 8) {
+            const sevenResult = storedData[storedData.length - 8];
+           sevenScore.textContent = `7 - you popped ${sevenResult} cat`;
+        } else {
+            sevenScore.style.display = "none";
+        }
 
-const fiveResult = storedData[storedData.length - 6];
-const fiveScore = document.querySelector(".five");
-fiveScore.textContent = `5  you popped ${fiveResult} cat`;
+        const eightScore = document.querySelector(".eight");
+        if (storedData.length >= 9) {
+            const eightResult = storedData[storedData.length - 9];
+            eightScore.textContent = `8 - you popped ${eightResult} cat`;
+        } else {
+            eightScore.style.display = "none";
+        }
 
-const sixResult = storedData[storedData.length - 7];
-const sixScore = document.querySelector(".six");
-sixScore.textContent = `6  you popped ${sixResult} cat`;
+        const nineScore = document.querySelector(".nine");
+        if (storedData.length >= 10) {
+           const nineResult = storedData[storedData.length - 10];
+           nineScore.textContent = `9 - you popped ${nineResult} cat`;
+        } else {
+            nineScore.style.display = "none";
+        }
+       
+        const tenScore = document.querySelector(".ten");
+        if (storedData.length >= 11) {
+            const tenResult = storedData[storedData.length - 11];
+            tenScore.textContent = `10 - you popped ${tenResult} cat`;
+        } else {
+            tenScore.style.display = "none";
+        }
 
-const sevenResult = storedData[storedData.length - 8];
-const sevenScore = document.querySelector(".seven");
-sevenScore.textContent = `7  you popped ${sevenResult} cat`;
-
-const eightResult = storedData[storedData.length - 9];
-const eightScore = document.querySelector(".eight");
-eightScore.textContent = `8  you popped ${eightResult} cat`;
-
-const nineResult = storedData[storedData.length - 10];
-const nineScore = document.querySelector(".nine");
-nineScore.textContent = `9  you popped ${nineResult} cat`;
-
-const tenResult = storedData[storedData.length - 11];
-const tenScore = document.querySelector(".ten");
-tenScore.textContent = `10  you popped ${tenResult} cat`;
     }
 }
 
 function pauseTimer() {
-    clearInterval(intervalId); // Остановить интервал
-    intervalId = null; // Установить интервал в null
+    clearInterval(intervalId);
+    intervalId = null;
     playButton.classList.remove("pause");
     playButton.classList.add("play");
     gamePlaying = false;
-    resetTimer(); // Сбросить таймер
+    resetTimer();
     score = 0;
     getScore();
 }
@@ -192,9 +217,8 @@ function resetTimer() {
     currentTime = 0;
     const progressTime = document.querySelector(".current");
     progressTime.textContent = getTime(currentTime);
-    
 }
-// Функция для форматирования времени
+
 function getTime(duration) {
     const minutes = Math.floor((duration % 3600) / 60);
     const seconds = Math.floor(duration % 60);
@@ -203,11 +227,8 @@ function getTime(duration) {
     return `${formattedMinutes}:${formattedSeconds}`;
 }
 
-
 const exit = document.querySelector(".exit");
 
 exit.addEventListener("click", () => {
     modalUp.style.display = "none";
 });
-
-
